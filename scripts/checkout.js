48ec29e73158bac0815c7000fb5148ff1427e724
LiangDingxuan <S10258272@connect.np.edu.sg>
2024-01-25 10:16:32 +0800

    cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,72 +1,43 @@
-<div class="container">
-    <div class="checkoutLayout">
+let listCart = [];
+function checkCart(){
+        var cookieValue = document.cookie
+        .split('; ')
+        .find(row => row.startsWith('listCart='));
+        if(cookieValue){
+            listCart = JSON.parse(cookieValue.split('=')[1]);
+        }
+}
+checkCart();
+addCartToHTML();
+function addCartToHTML(){
+    // clear data default
+    let listCartHTML = document.querySelector('.returnCart .list');
+    listCartHTML.innerHTML = '';
 
-        
-        <div class="returnCart">
-            <a href="/">keep shopping</a>
-            <h1>List Product in Cart</h1>
-            <div class="list">
-
-                <div class="item">
-                    <img src="images/1.webp">
+    let totalQuantityHTML = document.querySelector('.totalQuantity');
+    let totalPriceHTML = document.querySelector('.totalPrice');
+    let totalQuantity = 0;
+    let totalPrice = 0;
+    // if has product in Cart
+    if(listCart){
+        listCart.forEach(product => {
+            if(product){
+                let newCart = document.createElement('div');
+                newCart.classList.add('item');
+                newCart.innerHTML = 
+                    `<img src="${product.image}">
                     <div class="info">
-                        <div class="name">PRODUCT 1</div>
-                        <div class="price">$22/1 product</div>
+                        <div class="name">${product.name}</div>
+                        <div class="price">$${product.price}/1 product</div>
                     </div>
-                    <div class="quantity">5</div>
-                    <div class="returnPrice">$433.3</div>
-                </div>
-
-            </div>
-        </div>
-
-
-        <div class="right">
-            <h1>Checkout</h1>
-
-            <div class="form">
-                <div class="group">
-                    <label for="name">Full Name</label>
-                    <input type="text" name="name" id="name">
-                </div>
-    
-                <div class="group">
-                    <label for="phone">Phone Number</label>
-                    <input type="text" name="phone" id="phone">
-                </div>
-    
-                <div class="group">
-                    <label for="address">Address</label>
-                    <input type="text" name="address" id="address">
-                </div>
-    
-                <div class="group">
-                    <label for="country">Country</label>
-                    <select name="country" id="country">
-                        <option value="">Choose..</option>
-                        <option value="">Kingdom</option>
-                    </select>
-                </div>
-    
-                <div class="group">
-                    <label for="city">City</label>
-                    <select name="city" id="city">
-                        <option value="">Choose..</option>
-                        <option value="">London</option>
-                    </select>
-                </div>
-            </div>
-            <div class="return">
-                <div class="row">
-                    <div>Total Quantity</div>
-                    <div class="totalQuantity">70</div>
-                </div>
-                <div class="row">
-                    <div>Total Price</div>
-                    <div class="totalPrice">$900</div>
-                </div>
-            </div>
-            <button class="buttonCheckout">CHECKOUT</button>
-            </div>
-    </div>
-</div>
+                    <div class="quantity">${product.quantity}</div>
+                    <div class="returnPrice">$${product.price * product.quantity}</div>`;
+                listCartHTML.appendChild(newCart);
+                totalQuantity = totalQuantity + product.quantity;
+                totalPrice = totalPrice + (product.price * product.quantity);
+            }
+        })
+    }
+    totalQuantityHTML.innerText = totalQuantity;
+    totalPriceHTML.innerText = '$' + totalPrice;
+}
