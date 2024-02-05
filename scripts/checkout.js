1340c1dad51ce4ce9455240687e1a607583b3845
LiangDingxuan <S10258272@connect.np.edu.sg>
2024-02-05 22:46:34 +0800

    discount implemented

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -13,13 +13,52 @@ function addCartToHTML(){
     // clear data default
     let listCartHTML = document.querySelector('.returnCart .list');
     listCartHTML.innerHTML = '';
-
+    
     let totalQuantityHTML = document.querySelector('.totalQuantity');
     let totalPriceHTML = document.querySelector('.totalPrice');
+    let discountHTML = document.querySelector('.discount');
     let totalQuantity = 0;
     let totalPrice = 0;
+    let discount = 0;
     // if has product in Cart
     if(listCart){
+        //extracting discount
+        if (localStorage.getItem('isLoggedIn') === 'true'){
+            $.ajax({
+                type: "GET",
+                url: "https://fedproject-3bfe.restdb.io/rest/account",
+                headers: {
+                "Content-Type": "application/json",
+                "x-apikey": "65b1b9947d4b3ea75e7e0415",
+                },
+                success: function (data) {
+                let user = data.find(
+                    (user) => user.Email == localStorage.getItem('Email') && user.Password == localStorage.getItem('Password')
+                );
+                if (user) {
+                    let ObjID = user._id; // ObjectID
+                    let currentScore = user.Score;
+                    console.log("User ObjectID:", ObjID);
+                    console.log(user.Name);
+
+                    discount = currentScore * 0.10
+                    console.log(discount);
+
+                    discountHTML.innerText = '$' + discount;
+                } else {
+                    console.log("User not found");
+                }
+                
+              
+                },
+                error: function (error) {
+                console.log(error);
+                },
+            });
+        }
+
+
+        // writing checkout page html
         listCart.forEach(product => {
             if(product){
                 let newCart = document.createElement('div');
@@ -35,9 +74,11 @@ function addCartToHTML(){
                 listCartHTML.appendChild(newCart);
                 totalQuantity = totalQuantity + product.quantity;
                 totalPrice = totalPrice + (product.price * product.quantity);
+                
             }
         })
     }
     totalQuantityHTML.innerText = totalQuantity;
     totalPriceHTML.innerText = '$' + totalPrice;
+    
 }
